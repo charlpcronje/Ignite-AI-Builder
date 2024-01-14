98b888f0a9b694a3567c5fd74d656f18595d5951
Charl Cronje <charlcp@gmail.com>
2024-01-14 00:31:52 +0000

    Updated to lock files for multiple users

--- file_handler.py
+++ file_handler.py
@@ -3,6 +3,7 @@
 
 import json
 import logging
+import fcntl
 
 class FileHandler:
     def __init__(self):
@@ -10,7 +11,8 @@ class FileHandler:
 
     def read_json_file(self, file_path):
         """
-        Reads and returns the content of a JSON file.
+        Reads and returns the content of a JSON file, also locks the file so that it cannot be modified. 
+        This is to prevent concurrent access to the file.
         Args:
         - file_path (str): The path to the JSON file.
 
@@ -19,17 +21,21 @@ class FileHandler:
         """
         try:
             with open(file_path, 'r') as file:
-                return json.load(file)
+                fcntl.flock(file, fcntl.LOCK_SH)  # Shared lock for reading
+                data = json.load(file)
+                fcntl.flock(file, fcntl.LOCK_UN)  # Unlock the file
+                return data
         except FileNotFoundError:
             logging.error(f"File not found: {file_path}")
-            return None
         except json.JSONDecodeError as e:
             logging.error(f"Error decoding JSON from {file_path}: {e}")
-            return None
+        except Exception as e:
+            logging.error(f"Error reading file {file_path}: {e}")
+        return None
 
     def write_json_file(self, file_path, data):
         """
-        Writes the given data to a JSON file.
+        Writes the given data to a JSON file, also unlocks the file so that it can be modified.
 
         Args:
             file_path (str): The path to the JSON file.
@@ -37,7 +43,9 @@ class FileHandler:
         """
         try:
             with open(file_path, 'w') as file:
+                fcntl.flock(file, fcntl.LOCK_EX)  # Exclusive lock for writing
                 json.dump(data, file, indent=4)
+                fcntl.flock(file, fcntl.LOCK_UN)  # Unlock the file
             logging.info(f"Data successfully written to {file_path}")
         except Exception as e:
             logging.error(f"Failed to write data to {file_path}: {e}")
